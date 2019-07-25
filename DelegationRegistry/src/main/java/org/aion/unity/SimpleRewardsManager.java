bea576cf9ea4127cd6c947e2a2069a285a0cf1cc
ali-sharif <ali@aion.network>
2019-07-25 14:53:01 -0400

    simple rewards manager and test

---
+++ DelegationRegistry/src/main/java/org/aion/unity/SimpleRewardsManager.java
@@ -0,0 +1,96 @@
+package org.aion.unity;
+
+import avm.Address;
+
+import java.util.*;
+
+public class SimpleRewardsManager extends RewardsManager {
+
+    private long totalStake = 0L;
+    private long totalRewards = 0L;
+    private Map<Address, Long> stakeMap = new HashMap<>();
+    private Map<Address, Long> rewardsMap = new HashMap<>();
+
+    public Map<Address, Long> computeRewards(List<Event> events) throws RuntimeException {
+        // ASSUMPTIONS:
+        // 1. all events are sorted by block number
+        // 2. the block event is the last event for that particular block
+        // 3. event amount cannot be negative
+        // 4. vote events should not overflow long (not checked)
+        // 3. un-votes should not make a stake delegation negative
+        // 6. withdraw should not be called if no rewards exist
+        // 7. withdraw should not make my rewards balance negative
+
+        ListIterator<Event> itr = events.listIterator();
+
+        while (itr.hasNext()) {
+            Event x = itr.next();
+            if (x.amount < 0)
+                throw new RuntimeException("Event amount is negative.");
+
+            if (itr.hasNext() && events.get(itr.nextIndex()).blockNumber < x.blockNumber)
+                throw new RuntimeException("Block numbers are NOT monotonically increasing");
+
+            switch (x.type) {
+                case VOTE: {
+                    if (stakeMap.containsKey(x.source)) {
+                        stakeMap.put(x.source, stakeMap.get(x.source) + x.amount);
+                    } else {
+                        stakeMap.put(x.source, x.amount);
+                    }
+                    totalStake += x.amount;
+                    break;
+                }
+                case UNVOTE: {
+                    if (!stakeMap.containsKey(x.source))
+                        throw new RuntimeException("un-vote event called without any stake delegated.");
+
+                    long ns = stakeMap.get(x.source) - x.amount;
+                    if (ns < 0) throw new RuntimeException("Un-vote event made stake balance negative.");
+                    stakeMap.put(x.source, ns);
+
+                    totalStake -= x.amount;
+                    break;
+                }
+                case WITHDRAW: {
+                    if (!rewardsMap.containsKey(x.source))
+                        throw new RuntimeException("Withdraw event called without any rewards balance.");
+
+                    long ns = rewardsMap.get(x.source) - x.amount;
+                    if (ns < 0) throw new RuntimeException("Un-vote event made stake balance negative.");
+                    rewardsMap.put(x.source, ns);
+
+                    totalRewards -= x.amount;
+                    break;
+                }
+                case BLOCK: {
+                    if (itr.hasNext() && events.get(itr.nextIndex()).blockNumber <= x.blockNumber)
+                        throw new RuntimeException("Block event is NOT the last event for contained in this block!");
+
+                    // split the rewards for this block between the stakers who contributed to it.
+                    long blockReward = x.amount;
+
+                    // i need to compute the ratio of what is owed to each of the stakers
+                    Map<Address, Float> ratioOwed = new HashMap<>();
+                    for (Map.Entry<Address, Long> s : stakeMap.entrySet()) {
+                        ratioOwed.put(s.getKey(), (float)s.getValue()/totalStake);
+                    }
+
+                    for (Map.Entry<Address, Float> r : ratioOwed.entrySet()) {
+                        long stakerReward = (long) Math.floor(blockReward * r.getValue());
+                        if (rewardsMap.containsKey(r.getKey())) {
+                            rewardsMap.put(r.getKey(), rewardsMap.get(r.getKey()) + stakerReward);
+                        } else {
+                            rewardsMap.put(r.getKey(), stakerReward);
+                        }
+                    }
+                    break;
+                }
+                default:
+                    throw new RuntimeException("Event type not recognized.");
+            }
+        }
+
+        return rewardsMap;
+    }
+}
---
+++ DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
@@ -0,0 +1,83 @@
+package org.aion.unity;
+
+import avm.Address;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.aion.unity.RewardsManager.Event;
+import static org.aion.unity.RewardsManager.EventType;
+import static org.junit.Assert.fail;
+
+public class SimpleRewardsManagerTest {
+
+    private Address addressOf(int n) {
+        byte[] b = new byte[32];
+        b[0] = (byte) n;
+        return new Address(b);
+    }
+
+    final long REWARD = 5000;
+
+    @Test(expected = RuntimeException.class)
+    public void testBadInput1() {
+        // Order of blocks is wrong. Should throw a runtime exception.
+        List<Event> v = new ArrayList<>();
+        v.add(new Event(EventType.VOTE, addressOf(0), 1, 1));
+        v.add(new Event(EventType.VOTE, addressOf(1), 2, 1));
+        v.add(new Event(EventType.VOTE, addressOf(2), 3, 1));
+        v.add(new Event(EventType.VOTE, addressOf(3), 1, 1));
+
+        RewardsManager rm = new SimpleRewardsManager();
+        rm.computeRewards(v);
+    }
+
+    @Test
+    public void testBadInput2() {
+        // Order of blocks is wrong. Should throw a runtime exception.
+        List<Event> v = new ArrayList<>();
+        v.add(new Event(EventType.VOTE, addressOf(0), 1, 1));
+        v.add(new Event(EventType.VOTE, addressOf(1), 2, 1));
+        v.add(new Event(EventType.BLOCK, null, 3, 1));
+        v.add(new Event(EventType.VOTE, addressOf(2), 3, 1));
+
+        RewardsManager rm = new SimpleRewardsManager();
+        boolean a = true;
+        try {
+            a = false;
+            rm.computeRewards(v);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        if (a) fail("Exception not thrown");
+    }
+
+
+    @Test
+    public void testVector001Simple() {
+        // Generate test vector
+        List<Event> v = new ArrayList<>();
+        v.add(new Event(EventType.VOTE, addressOf(1), 1, 3400));
+        v.add(new Event(EventType.VOTE, addressOf(2), 1, 5700));
+        v.add(new Event(EventType.VOTE, addressOf(2), 2, 2000));
+        v.add(new Event(EventType.BLOCK, null, 2, REWARD));
+        v.add(new Event(EventType.VOTE, addressOf(3), 3, 6000));
+        v.add(new Event(EventType.VOTE, addressOf(4), 4, 1500));
+        v.add(new Event(EventType.VOTE, addressOf(5), 4, 6000));
+        v.add(new Event(EventType.BLOCK, null, 7, REWARD));
+        v.add(new Event(EventType.BLOCK, null, 8, REWARD));
+        v.add(new Event(EventType.BLOCK, null, 11, REWARD));
+        //v.add(new Event(EventType.WITHDRAW, addressOf(2), 12, 100));
+        //v.add(new Event(EventType.WITHDRAW, addressOf(3), 12, 100));
+        //v.add(new Event(EventType.WITHDRAW, addressOf(4), 12, 100));
+
+        RewardsManager rm = new SimpleRewardsManager();
+        Map<Address, Long> r = rm.computeRewards(v);
+        System.out.println("testVector001Simple");
+    }
+
+
+}
