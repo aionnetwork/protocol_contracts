674a61dbf757024209af1c6e92ae3c6f64cfc644
ali-sharif <ali@aion.network>
2019-07-25 16:53:57 -0400

    changed the simple rewards manager to return withdrawn + pending rewards

--- DelegationRegistry/src/main/java/org/aion/unity/SimpleRewardsManager.java
+++ DelegationRegistry/src/main/java/org/aion/unity/SimpleRewardsManager.java
@@ -7,9 +7,10 @@ import java.util.*;
 public class SimpleRewardsManager extends RewardsManager {
 
     private long totalStake = 0L;
-    private long rewardsOutstanding = 0L;
+    private long rewardOutstanding = 0L;
     private Map<Address, Long> stakeMap = new HashMap<>();
-    private Map<Address, Long> rewardsMap = new HashMap<>();
+    private Map<Address, Long> rewardMap = new HashMap<>();
+    private Map<Address, Long> withdrawnMap = new HashMap<>();
 
     public Map<Address, Long> computeRewards(List<Event> events) throws RuntimeException {
         // ASSUMPTIONS:
@@ -53,14 +54,21 @@ public class SimpleRewardsManager extends RewardsManager {
                     break;
                 }
                 case WITHDRAW: {
-                    if (!rewardsMap.containsKey(x.source))
+                    if (!rewardMap.containsKey(x.source))
                         throw new RuntimeException("Withdraw event called without any rewards balance.");
 
-                    long ns = rewardsMap.get(x.source) - x.amount;
+                    long ns = rewardMap.get(x.source) - x.amount;
                     if (ns < 0) throw new RuntimeException("Un-vote event made stake balance negative.");
-                    rewardsMap.put(x.source, ns);
 
-                    rewardsOutstanding -= x.amount;
+                    rewardMap.put(x.source, ns);
+                    rewardOutstanding -= x.amount;
+
+                    if (withdrawnMap.containsKey(x.source)) {
+                        withdrawnMap.put(x.source, withdrawnMap.get(x.source) + x.amount);
+                    } else {
+                        withdrawnMap.put(x.source, x.amount);
+                    }
+
                     break;
                 }
                 case BLOCK: {
@@ -69,7 +77,7 @@ public class SimpleRewardsManager extends RewardsManager {
 
                     // split the rewards for this block between the stakers who contributed to it.
                     long blockReward = x.amount;
-                    rewardsOutstanding += blockReward;
+                    rewardOutstanding += blockReward;
 
                     // i need to compute the ratio of what is owed to each of the stakers
                     Map<Address, Float> ratioOwed = new HashMap<>();
@@ -79,10 +87,10 @@ public class SimpleRewardsManager extends RewardsManager {
 
                     for (Map.Entry<Address, Float> r : ratioOwed.entrySet()) {
                         long stakerReward = (long) Math.floor(blockReward * r.getValue());
-                        if (rewardsMap.containsKey(r.getKey())) {
-                            rewardsMap.put(r.getKey(), rewardsMap.get(r.getKey()) + stakerReward);
+                        if (rewardMap.containsKey(r.getKey())) {
+                            rewardMap.put(r.getKey(), rewardMap.get(r.getKey()) + stakerReward);
                         } else {
-                            rewardsMap.put(r.getKey(), stakerReward);
+                            rewardMap.put(r.getKey(), stakerReward);
                         }
                     }
                     break;
@@ -92,6 +100,31 @@ public class SimpleRewardsManager extends RewardsManager {
             }
         }
 
-        return rewardsMap;
+        Map<Address, Long> toReturnMap = new HashMap<>();
+        rewardMap.forEach((k, v) -> {
+            toReturnMap.put(k, v + withdrawnMap.get(k));
+        });
+        return toReturnMap;
+    }
+
+    /**
+     * @return clone of the stakeMap (local state)
+     */
+    public Map<Address, Long> getStakeMap() {
+        return new HashMap<>(stakeMap);
+    }
+
+    /**
+     * @return clone of the rewardMap (local state)
+     */
+    public Map<Address, Long> getRewardMap() {
+        return new HashMap<>(rewardMap);
+    }
+
+    /**
+     * @return clone of the rewardMap (local state)
+     */
+    public long getRewardOutstanding() {
+        return rewardOutstanding;
     }
 }
--- DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
+++ DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import static org.aion.unity.RewardsManager.Event;
 import static org.aion.unity.RewardsManager.EventType;
@@ -19,7 +20,7 @@ public class SimpleRewardsManagerTest {
         return new Address(b);
     }
 
-    final long REWARD = 5000;
+
 
     @Test(expected = RuntimeException.class)
     public void testBadInput1() {
@@ -58,6 +59,8 @@ public class SimpleRewardsManagerTest {
 
     @Test
     public void testVector001Simple() {
+        final long REWARD = 5000;
+
         // Generate test vector
         List<Event> v = new ArrayList<>();
         v.add(new Event(EventType.VOTE, addressOf(1), 1, 3400));
@@ -76,13 +79,75 @@ public class SimpleRewardsManagerTest {
         v.add(new Event(EventType.VOTE, addressOf(5), 12, 1200));
         v.add(new Event(EventType.UNVOTE, addressOf(2), 12, 3000));
         v.add(new Event(EventType.BLOCK, null, 25, REWARD));
-        v.add(new Event(EventType.WITHDRAW, addressOf(5), 26, 5000));
+        v.add(new Event(EventType.WITHDRAW, addressOf(5), 26, 5235));
+        v.add(new Event(EventType.BLOCK, null, 32, REWARD));
+        v.add(new Event(EventType.WITHDRAW, addressOf(4), 36, 1068));
+        v.add(new Event(EventType.WITHDRAW, addressOf(5), 36, 1578));
 
         RewardsManager simple = new SimpleRewardsManager();
         Map<Address, Long> r0 = simple.computeRewards(v);
 
-        RewardsManager dp = new DPRewardsManager();
-        Map<Address, Long> r1 = dp.computeRewards(v);
+        //RewardsManager dp = new DPRewardsManager();
+        //Map<Address, Long> r1 = dp.computeRewards(v);
+
+        System.out.println("testVector001Simple");
+    }
+
+    private int getRandomInt(int min, int max) {
+        return (int)(Math.random() * max) + min;
+    }
+
+    private boolean coinWithProbability(float probability) {
+        return (new Random().nextFloat() < probability);
+    }
+
+    @Test
+    public void testVectorAutogen() {
+        // generate test vector
+        List<Event> v = new ArrayList<>();
+
+        // system params
+        int users = 5;
+        long maxUserBalance = 25000;
+        long startBlock = 1;
+        long endBlock = 100;
+        int maxActionsPerBlock = 3;
+        long blockReward = 5000;
+        float poolProbability = 0.8f; // pool's probability of winning a block.
+
+        // abuse the simple rewards manager a little bit, by running it every time we add a new entry,
+        // to see what we can do next. probably a much better way to do this, but whatever ...
+        for(long i = startBlock; i <= endBlock; i++) {
+
+            // pick a certain number of actions to do in this round
+            int numActions = getRandomInt(0, maxActionsPerBlock);
+            for (int j = 0; j < numActions; j++) {
+
+                SimpleRewardsManager rm = new SimpleRewardsManager();
+                rm.computeRewards(v);
+
+                // choose a random user
+                Address user = addressOf(getRandomInt(1, users));
+
+                // choose a random action to do for the user
+                if (rm.getStakeMap().get(user) > 0) {
+
+                }
+
+
+            }
+
+            // decide if this block will be produced by this pool
+            if (coinWithProbability(poolProbability)) {
+                v.add(new Event(EventType.BLOCK, null, i, blockReward));
+            }
+        }
+
+        RewardsManager simple = new SimpleRewardsManager();
+        Map<Address, Long> r0 = simple.computeRewards(v);
+
+        //RewardsManager dp = new DPRewardsManager();
+        //Map<Address, Long> r1 = dp.computeRewards(v);
 
         System.out.println("testVector001Simple");
     }
