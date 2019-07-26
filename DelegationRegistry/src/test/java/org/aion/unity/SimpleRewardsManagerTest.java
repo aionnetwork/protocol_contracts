a67b487018a50ea180bb166a61357e3f817d40f5
ali-sharif <ali@aion.network>
2019-07-26 13:22:26 -0400

    events autogen enforcing pre-lock

--- DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
+++ DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
@@ -211,6 +211,120 @@ public class SimpleRewardsManagerTest {
                 100.0 * Math.abs(sum0 - sum) / sum, 100.0 * Math.abs(sum1 - sum) / sum);
     }
 
+    @Test
+    public void testVectorAutogenWithPreLockout() {
+        // generated vector
+        List<Event> q = new ArrayList<>();
+
+        // system params
+        int users = 10;
+        long maxUserBalance = 25000;
+        long startBlock = 1;
+        long endBlock = 1000;
+        int maxActionsPerBlock = 3;
+        long blockReward = 5_000_000L;
+        float poolProbability = 0.8f; // pool's probability of winning a block.
+        long preLockBlocks = 7;
+
+        SimpleRewardsManager rm = new SimpleRewardsManager();
+
+        Map<Address, Long> isUserLocked = new HashMap<>();
+
+        // abuse the simple rewards manager a little bit, by running it every time we add a new entry,
+        // to see what we can do next. probably a much better way to do this, but whatever ...
+        for (long i = startBlock; i <= endBlock; i++) {
+
+            // temporary list where we accumuate the events in this block
+            List<Event> v = new ArrayList<>();
+
+            Set<Address> usersInThisRound = new HashSet<>();
+
+            // pick a certain number of actions to do in this round
+            int numActions = getRandomInt(0, maxActionsPerBlock);
+            for (int j = 0; j < numActions; j++) {
+
+                // choose a random user, not already used in this round
+                Address user;
+                do {
+                    user = addressOf(getRandomInt(1, users));
+                } while (usersInThisRound.contains(user));
+                usersInThisRound.add(user);
+
+                // choose a random action to do for the user ...
+
+                // if the user hasn't staked already start with that
+                if (!rm.getStakeMap().containsKey(user)) {
+                    v.add(new Event(EventType.VOTE, user, i, getRandomLong(1, maxUserBalance)));
+                    isUserLocked.put(user, preLockBlocks);
+                } else {
+                    // choose between withdraw, unvote or vote more, with 1/3 probability
+                    int choice = getRandomInt(1, 3);
+                    Long stakedBalance = rm.getStakeMap().get(user);
+                    if (stakedBalance == null) stakedBalance = 0L;
+
+                    if (choice == 1) { // vote more
+                        long remainingBalance = maxUserBalance - stakedBalance;
+                        v.add(new Event(EventType.VOTE, user, i, getRandomLong(1, remainingBalance)));
+                        isUserLocked.put(user, preLockBlocks);
+                    } else if (choice == 2 && isUserLocked.get(user) <= 0) { // unvote
+                        if (stakedBalance > 0L)
+                            v.add(new Event(EventType.UNVOTE, user, i, getRandomLong(1, stakedBalance)));
+                    } else if (stakedBalance > 0L && isUserLocked.get(user) <= 0) { // withdraw
+                        Long pendingRewards = rm.getPendingRewardMap().get(user);
+                        if (pendingRewards == null) pendingRewards = 0L;
+
+                        // with half probability, withdraw all or some fraction
+                        boolean withdrawAll = coinWithProbability(0.5f);
+                        if (pendingRewards > 0) {
+                            if (withdrawAll) {
+                                v.add(new Event(EventType.WITHDRAW, user, i, pendingRewards));
+                            } else {
+                                v.add(new Event(EventType.WITHDRAW, user, i, getRandomLong(1, pendingRewards)));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // decide if this block will be produced by this pool
+            if (coinWithProbability(poolProbability)) {
+                v.add(new Event(EventType.BLOCK, null, i, blockReward));
+            }
+
+            // apply the rewards this round to the simple rewards manager
+            try {
+                rm.computeRewards(v);
+            } catch (Exception e) {
+                System.out.println("Bad events-list constructed.");
+            }
+
+            q.addAll(v);
+
+            // decrement the "pre-lock" period for the user to make a withdrawal or unvote
+            isUserLocked.forEach((x, y) -> {
+                isUserLocked.put(x,  y-1 < 0 ? 0 : y-1);
+            });
+        }
+
+        RewardsManager simple = new SimpleRewardsManager();
+        Map<Address, Long> r0 = simple.computeRewards(q);
+        System.out.println(r0);
+
+        /*
+        RewardsManager dp = new DPRewardsManager();
+        Map<Address, Long> r1 = dp.computeRewards(q);
+        System.out.println(r1);
+
+        double[] error = calcErrorSD(r0, r1);
+        System.out.printf("Error: mean = %.2f%%, sd = %.2f%%\n", error[0], error[1]);
+
+        long sum = q.stream().filter(e -> e.type == EventType.BLOCK).mapToLong(e -> e.amount).sum();
+        long sum0 = r0.values().stream().mapToLong(v -> v).sum();
+        long sum1 = r1.values().stream().mapToLong(v -> v).sum();
+        System.out.printf("Sum = [%d, %d], lose = [%.2f%%, %.2f%%]\n", sum0, sum1,
+                100.0 * Math.abs(sum0 - sum) / sum, 100.0 * Math.abs(sum1 - sum) / sum);
+        */
+    }
 
     // assuming key sets are the same
     private double[] calcErrorSD(Map<Address, Long> base, Map<Address, Long> toCheck) {
