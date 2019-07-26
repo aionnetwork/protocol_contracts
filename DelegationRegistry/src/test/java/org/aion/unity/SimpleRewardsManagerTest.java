6759e531390205d2f60777bc9a292bfed6270064
ali-sharif <ali@aion.network>
2019-07-26 11:06:10 -0400

    bug fixes on the test vector autogenerator

--- DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
+++ DelegationRegistry/src/test/java/org/aion/unity/SimpleRewardsManagerTest.java
@@ -3,10 +3,8 @@ package org.aion.unity;
 import avm.Address;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.math.BigInteger;
+import java.util.*;
 
 import static org.aion.unity.RewardsManager.Event;
 import static org.aion.unity.RewardsManager.EventType;
@@ -16,7 +14,12 @@ public class SimpleRewardsManagerTest {
 
     private Address addressOf(int n) {
         byte[] b = new byte[32];
-        b[0] = (byte) n;
+
+        byte[] trimmed = BigInteger.valueOf(n).toByteArray();
+        int padding = 32 - trimmed.length;
+
+        System.arraycopy(trimmed, 0, b, padding, trimmed.length);
+
         return new Address(b);
     }
 
@@ -97,6 +100,10 @@ public class SimpleRewardsManagerTest {
         return (int)(Math.random() * max) + min;
     }
 
+    private long getRandomLong(long min, long max) {
+        return (long)(Math.random() * max) + min;
+    }
+
     private boolean coinWithProbability(float probability) {
         return (new Random().nextFloat() < probability);
     }
@@ -107,10 +114,10 @@ public class SimpleRewardsManagerTest {
         List<Event> v = new ArrayList<>();
 
         // system params
-        int users = 5;
+        int users = 8;
         long maxUserBalance = 25000;
         long startBlock = 1;
-        long endBlock = 100;
+        long endBlock = 200;
         int maxActionsPerBlock = 3;
         long blockReward = 5000;
         float poolProbability = 0.8f; // pool's probability of winning a block.
@@ -124,17 +131,53 @@ public class SimpleRewardsManagerTest {
             for (int j = 0; j < numActions; j++) {
 
                 SimpleRewardsManager rm = new SimpleRewardsManager();
-                rm.computeRewards(v);
-
-                // choose a random user
-                Address user = addressOf(getRandomInt(1, users));
-
-                // choose a random action to do for the user
-                if (rm.getStakeMap().get(user) > 0) {
-
+                try {
+                    rm.computeRewards(v);
+                } catch (Exception e) {
+                    System.out.println("Bad events-list constructed.");
                 }
 
 
+                Set<Address> usersInThisRound = new HashSet<>();
+                // choose a random user, not already used in this round
+                Address user;
+                do {
+                    user = addressOf(getRandomInt(1, users));
+                } while (usersInThisRound.contains(user));
+                usersInThisRound.add(user);
+
+                // choose a random action to do for the user ...
+
+                // if the user hasn't staked already start with that
+                if (!rm.getStakeMap().containsKey(user)) {
+                    v.add(new Event(EventType.VOTE, user, i, getRandomLong(1, maxUserBalance)));
+                } else {
+                    // choose between withdraw, unvote or vote more, with 1/3 probability
+                    int choice = getRandomInt(1,3);
+                    Long stakedBalance = rm.getStakeMap().get(user);
+                    if (stakedBalance == null) stakedBalance = 0L;
+
+                    if (choice == 1) { // vote more
+                        long remainingBalance = maxUserBalance - stakedBalance;
+                        v.add(new Event(EventType.VOTE, user, i, getRandomLong(1, remainingBalance)));
+                    } else if (choice == 2) { // unvote
+                        if (stakedBalance > 0L)
+                            v.add(new Event(EventType.UNVOTE, user, i, getRandomLong(1, stakedBalance)));
+                    } else if (stakedBalance > 0L) { // withdraw
+                        Long pendingRewards = rm.getPendingRewardMap().get(user);
+                        if (pendingRewards == null) pendingRewards = 0L;
+
+                        // with half probability, withdraw all or some fraction
+                        boolean withdrawAll = coinWithProbability(0.5f);
+                        if (pendingRewards > 0) {
+                            if (withdrawAll) {
+                                v.add(new Event(EventType.WITHDRAW, user, i, pendingRewards));
+                            } else {
+                                v.add(new Event(EventType.WITHDRAW, user, i, getRandomLong(1, pendingRewards)));
+                            }
+                        }
+                    }
+                }
             }
 
             // decide if this block will be produced by this pool
