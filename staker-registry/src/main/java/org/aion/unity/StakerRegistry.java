8e4691e256d6bc25e3d4b33f61f1270e23e08ae1
Yulong Wu <yulong@aion.network>
2019-07-29 12:03:39 -0400

    Specifiy the interfaces of staker registry

--- staker-registry/src/main/java/org/aion/unity/StakerRegistry.java
+++ staker-registry/src/main/java/org/aion/unity/StakerRegistry.java
@@ -5,50 +5,115 @@ import avm.Address;
 import java.math.BigInteger;
 
 /**
- * A collection of functions supported by the staking registry contract.
+ * A staker registry manages the registration of stakers, and provides an interface for coin-holders
+ * to vote/unvote for a staker.
  */
 public interface StakerRegistry {
 
+    // TODO: use BigInteger for stake
+
     /**
-     * Returns the stake information.
+     * Registers a staker. The caller address will be the identification
+     * address of a registered staker.
      *
-     * <ul>
-     * <li>Owner/staker address - address of the staker owner, used to identify a staker, immutable</li>
-     * <li>Signing address - address of the signing key</li>
-     * <li>Coinbase address - address for collecting block rewards</li>
-     * </ul>
-     * <p>
-     * Note: ideally, the return value should be an object; using an array as custom data
-     * structure is not yet supported by ABI.
+     * @param signingAddress  the address of key used for signing a PoS block
+     * @param coinbaseAddress the address of key used for collecting block rewards
+     * @return true if successful, otherwise false
+     */
+    boolean registerStaker(Address signingAddress, Address coinbaseAddress);
+
+    /**
+     * Votes for a staker. Any liquid coins, passed along the call, become locked stake.
      *
      * @param staker the address of the staker
-     * @return a data structure, [owner address, signing address, coinbase address].
+     * @return true if successful, otherwise false
      */
-    Address[] getStakerAddresses(Address staker);
+    boolean vote(Address staker);
 
     /**
-     * Votes for a staker.
-     *
-     * @param staker the address of the staker to vote
-     */
-    void vote(Address staker);
-
-
-    /**
-     * Un-votes for a staker.
+     * Unvotes for a staker. After a successful unvote, the locked coins will be released
+     * to the original owners, subject to lock-up period.
      *
      * @param staker the address of the staker
      * @param amount the amount of stake
+     * @return true if successful, otherwise false
      */
-    void unvote(Address staker, BigInteger amount);
-
+    boolean unvote(Address staker, long amount);
 
     /**
-     * Un-votes for a staker, and release the funds to another address.
+     * Un-votes for a staker, and receives the released fund using another account.
      *
      * @param staker   the address of the staker
      * @param amount   the amount of stake
-     * @param receiver
+     * @param receiver the receiving addresss
+     * @return true if successful, otherwise false
      */
-    void unvoteTo(Address staker, BigInteger amount, Address receiver);
+    boolean unvoteTo(Address staker, long amount, Address receiver);
+
+
+    /**
+     * Transfers stake from one staker to another staker.
+     *
+     * @param fromStaker the address of the staker to transfer stake from
+     * @param toStaker   the address of the staker to transfer stake to
+     * @param amount     the amount of stake
+     * @return true if successful, otherwise false
+     */
+    boolean transferStake(Address fromStaker, Address toStaker, long amount);
+
+    /**
+     * Returns the total stake associated with a staker.
+     *
+     * @param staker the address of the staker
+     * @return the total amount of stake
+     */
+    long getTotalStake(Address staker);
+
+    /**
+     * Returns the stake from a voter to a staker.
+     *
+     * @param staker the address of the staker
+     * @param voter  the address of the staker
+     * @return the amount of stake
+     */
+    long getStake(Address staker, Address voter);
+
+    /**
+     * Returns the signing address of a staker.
+     *
+     * @param staker the address of the staker.
+     * @return the signing address
+     */
+    Address getSigningAddress(Address staker);
+
+    /**
+     * Returns the coinbase address of a staker.
+     *
+     * @param staker the address of the staker.
+     * @return the coinbase address
+     */
+    Address getCoinbaseAddress(Address staker);
+
+    /**
+     * Updates the signing address of a staker. Owner only.
+     *
+     * @param staker            the address of the staker
+     * @param newSigningAddress the new signing address
+     */
+    void setSigningAddress(Address staker, Address newSigningAddress);
+
+    /**
+     * Updates the coinbase address of a staker. Owner only.
+     *
+     * @param staker             the address of the staker
+     * @param newCoinbaseAddress the new coinbase address
+     */
+    void setCoinbaseAddress(Address staker, Address newCoinbaseAddress);
+
+    /**
+     * Registers a listener. Owner only.
+     *
+     * @param listener the address of the listener contract
+     */
+    void registerListener(Address listener);
 }
--- staker-registry/src/main/java/org/aion/unity/StakerRegistryListener.java
+++ staker-registry/src/main/java/org/aion/unity/StakerRegistryListener.java
@@ -6,13 +6,7 @@ import java.math.BigInteger;
 
 public interface StakerRegistryListener {
 
-    /**
-     * When the coinbase address of a staker is changed.
-     *
-     * @param staker             the staker address
-     * @param newCoinbaseAddress the new coinbase address
-     */
-    void onStakerCoinbaseKeyChange(Address staker, Address newCoinbaseAddress);
+    // TODO: use BigInteger for block rewards
 
     /**
      * When the signing address of a staker is changed.
@@ -20,14 +14,23 @@ public interface StakerRegistryListener {
      * @param staker            the staker address
      * @param newSigningAddress the new signing address
      */
-    void onStakerSigningKeyChange(Address staker, Address newSigningAddress);
+    void onSigningAddressChange(Address staker, Address newSigningAddress);
+
+    /**
+     * When the coinbase address of a staker is changed.
+     *
+     * @param staker             the staker address
+     * @param newCoinbaseAddress the new coinbase address
+     */
+    void onCoinbaseAddressChange(Address staker, Address newCoinbaseAddress);
 
     /**
      * When a PoS block is created.
      *
      * @param staker  the staker address
+     * @param number  the block number
      * @param hash    the block hash
      * @param rewards the block rewards, including transaction fees collected
      */
-    void onBlockProduced(byte[] staker, byte[] hash, BigInteger rewards);
+    void onBlockProduction(Address staker, long number, byte[] hash, long rewards);
 }
